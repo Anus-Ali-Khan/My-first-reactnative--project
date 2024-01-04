8187a566cb54d54782e9dd2fd2038098c864c651
Anus Ali Khan <anusalikhan1405@gmail.com>
2024-01-04 23:38:10 +0500

    working on todo list

--- App.js
+++ App.js
@@ -1,56 +1,26 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
-  const [people, setPeople] = useState([
-    { name: "shaun", id: 1 },
-    { name: "yoshi", id: 2 },
-    { name: "mario", id: 3 },
-    { name: "luigi", id: 4 },
-    { name: "peach", id: 5 },
-    { name: "toad", id: 6 },
-    { name: "bowser", id: 7 },
+  const [todos, setTodos] = useState([
+    { text: "buy coffee", key: "1" },
+    { text: "create an app", key: "2" },
+    { text: "play on the switch", key: "3" },
   ]);
-
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter((person) => person.id !== id);
-    });
-  };
-
   return (
     <View style={styles.container}>
-      <FlatList
-        // numColumns={2} // it only works when you replace keyExtractor with key={same as numColumns value}
-        // keyExtractor={(item) => item.id}
-        numColumns={2}
-        key={2}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-
-      {/* <ScrollView>
-        {people.map((item) => {
-          return (
-            <View key={item.key}>
-              <Text style={styles.item}>{item.name}</Text>
-            </View>
-          );
-        })}
-      </ScrollView> */}
-      {/* <StatusBar style="auto" /> */}
+      {/* header */}
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => <Text>{item.text}</Text>}
+          />
+        </View>
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -59,17 +29,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    // alignItems: "center",
-    // justifyContent: "center",
   },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: "pink",
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24,
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   },
 });
